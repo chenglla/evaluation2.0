a134bc8e9bfc2cc3f55b287b742dad0ccc546e6f
Zn139 <1395692842@qq.com>
2020-06-05 18:53:29 +0800

    职业案例等文件的提交

--- src/careerCase/index.vue
+++ src/careerCase/index.vue
@@ -1,123 +1,71 @@
 <template>
   <div class="pushList">
     <div class="push_header">
-      <div class="title">文章推送</div>
+      <div class="title">职业案例</div>
     </div>
     <div class="search">
       <input type="text" placeholder='搜索关键词' >
       <i class="iconfont iconsousuo"></i>
     </div>
     <div class="push_split"></div>
-    <div class="w1">
-      <div class="push-middle" ref="wrapper">
-          <div class="push-content" >
-            <div class="push-list" v-for="(pushlist,index) in pushlists" :key="index">
-              <div>
-                <img src="../assets/img/火苗.png" alt="">
-              </div>
-              <div>
-                <h4>{{pushlist.title}}</h4>
-                <span>{{pushlist.source}}</span>
-                <span>{{pushlist.school}}</span>
-                <span class="pushtime">{{pushlist.time}}</span>
-              </div>
+    <div class="push-middle" ref="pushWrapper">
+        <div class="push-content" style="padding: 20px 0 5px 0;">
+
+          <div class="push-list" v-for="(caseList,index) in caseLists" :key="index" @click="toCaseDetail(caseList.caseId)">
+            <div>
+              <img src="../assets/img/火苗.png" alt="">
+            </div>
+            <div>
+              <h4>{{caseList.caseName}}</h4>
+              <!-- <span>{{caseLists.source}}</span>
+              <span>{{caseLists.school}}</span>
+              <span class="pushtime">{{caseLists.time}}</span> -->
             </div>
           </div>
         </div>
-
     </div>
   </div>
 </template>
 <script>
+import {findCaseList} from '@/api/index'
 import BScroll from 'better-scroll'
-//let wrapper = document.querySelector('.wrapper')
-//let pushScroll = new BScroll(wrapper,{})
 export default {
   data () {
     return {
       pushScroll: null,
-      pushlists:[
-        {
-          title:'南京工业大学浦江学院2020年本科生招生简章',
-          source:'来源',
-          school:'南京工业大学',
-          time:'2020-04-21',
-        },
-		{
-		  title:'南京工业大学浦江学院2020年本科生招生简章',
-		  source:'来源',
-		  school:'南京工业大学',
-		  time:'2020-04-21',
-		},
-		{
-		  title:'南京工业大学浦江学院2020年本科生招生简章',
-		  source:'来源',
-		  school:'南京工业大学',
-		  time:'2020-04-21',
-		},
-		{
-		  title:'南京工业大学浦江学院2020年本科生招生简章',
-		  source:'来源',
-		  school:'南京工业大学',
-		  time:'2020-04-21',
-		},
-		{
-		  title:'南京工业大学浦江学院2020年本科生招生简章',
-		  source:'来源',
-		  school:'南京工业大学',
-		  time:'2020-04-21',
-		},
-        {
-          title:'南京工业大学浦江学院2020年本科生招生简章',
-          source:'来源',
-          school:'南京工业大学',
-          time:'2020-04-21',
-        },
-        {
-          title:'南京工业大学浦江学院2020年本科生招生简章',
-          source:'来源',
-          school:'南京工业大学',
-          time:'2020-04-21',
-        },
-        {
-          title:'南京工业大学浦江学院2020年本科生招生简章',
-          source:'来源',
-          school:'南京工业大学',
-          time:'2020-04-21',
-        },
-        {
-          title:'南京工业大学浦江学院2020年本科生招生简章',
-          source:'来源',
-          school:'南京工业大学',
-          time:'2020-04-21',
-        },
-        {
-          title:'南京工业大学浦江学院2020年本科生招生简章',
-          source:'来源',
-          school:'南京工业大学',
-          time:'2020-04-21',
-        },
-        {
-          title:'南京工业大学浦江学院2020年本科生招生简章',
-          source:'来源',
-          school:'南京工业大学',
-          time:'2020-04-21',
-        }
-      ]
-
+      caseLists:[]
     }
   },
-
+  mounted () {
+    this.findCaseList ()
+    this.init()
+  },
   methods: {
-    initRight () {
+    init () {
       this.$nextTick(() => {
-        this.pushScroll = new BScroll(this.$refs.wrapper, {
+        this.pushScroll = new BScroll(this.$refs.pushWrapper, {
           click: true,
-          scrollY:true,
+          bounce: false,
         })
+        //console.log(this.pushScroll)
       })
     },
-
+    findCaseList () {
+      findCaseList({
+      }).then(res => {
+        
+        this.caseLists = res.data
+      })
+    },
+    toCaseDetail (id) {
+      console.log('id:', id)
+      this.$router.push({
+        path: '/caseDetail',
+        query: {
+          id: id
+        }
+      })
+    },
   }
 }
 </script>
@@ -132,7 +80,7 @@ export default {
   // 标题部分
   .push_header {
     position: relative;
-    font-size: 16px;
+    font-size: 20px;
     width: 100%;
     background: linear-gradient(to right, #00d2ff 0%, #37a3ff 100%);
     /*background-color: #19bdff;*/
@@ -154,19 +102,18 @@ export default {
   .search{
     border: solid 1px white;
     z-index: 1;
-    margin: 5px 18px;
+    margin: 10px 18px;
     border-radius: 4px;
     position: relative;
+    padding: 6px 0;
     input{
       color: white;
+      width: 70vw;
       border: 0;
       padding-left: 15px;
-      font-size: 8px;
+      font-size: 14px;
       // 背景透明度
       background-color: rgba(0,0,0,0);
-      padding-bottom: 8px;
-      //background-color: green;
-
     }
     input[type=text]:focus{
       outline: none;
@@ -174,15 +121,15 @@ export default {
     //修改placeholder的样式
     input::-webkit-input-placeholder{
       color: white;
+      font-size: 14px;
     }
-
     .iconsousuo{
+      font-size: 1.25rem;
       color: white;
       position: absolute;
       right: 15px;
       top: 4px;
     }
-
   }
   // 蓝色部分背景
   .push_split {
@@ -194,44 +141,38 @@ export default {
     width: 100%;
   }
   // 中间的百科列表
-
   .push-middle {
     position: absolute;
-    width:calc(100% - 30px);
-    top: 90px;
-    padding-top: 18px;
+    width:calc(100% - 62px);
+    top: 7.25rem;
+    //padding-top: 40px;
+    padding: 0 16px 0 16px;
     z-index: 1;
     border-radius: 4px;
-    display: flex;
-
     background-color: white;
-    margin: 5px 15px 5px;
-    height: calc(100% - 70px);
+    margin: 0 15px;
+    height: calc(100% - 145px);
     overflow: hidden;
-
-  }
-  .push-content{
-    flex: 1;
-    //height: auto;
-    padding: 0 15px;
   }
   // 具体列表
   .push-list{
-    position: relative;
-    width: 82vw;
+    //height: 30px;
+    //padding: 0 15px 3px 5px;
+    //position: relative;
+    //width: calc(100% - 30px);
     border-bottom: solid 1px #F7F7F7;
-    font-size: 14px;
+    font-size: 16px;
     color: #525252;
-    margin-bottom: 8px;
-    padding-bottom: 2px;
-
+    margin-bottom: 15px;
+    padding-bottom: 6px;
     display: flex;
+    //background-color: green;
     push-list .img{
       width: 3px;
       height: 3px;
     }
     span{
-      font-size: 8px;
+      font-size: 12px;
       color: rgb(181,181,181);
     }
   }
@@ -239,7 +180,6 @@ export default {
     position: absolute;
     right: 10px;
   }
-
   h4{
     color: rgb(84,84,84);
     margin-left: 6px;
